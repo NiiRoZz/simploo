5da5dacbb35e4ac55f7a03ef290f3015d2b6ea29
NiiRoZz <extazziii1000@gmail.com>
2020-12-21 10:15:10 +0100

    Fix unpack with lua version more than 5.2

--- util/luaunit.lua
+++ util/luaunit.lua
@@ -659,7 +659,7 @@ TextOutput_MT = { -- class
     end
 
     function TextOutput:displayOneFailedTest( failure )
-        testName, errorMsg, stackTrace = unpack( failure )
+        testName, errorMsg, stackTrace = table.unpack( failure )
         print(">>> "..testName.." failed")
         print( errorMsg )
         if self.verbosity > VERBOSITY_DEFAULT then
